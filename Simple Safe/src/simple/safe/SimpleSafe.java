865528e2b725e11b826074696ae0b2626442b675
Willy-557 <williamfsetiadi@gmail.com>
2026-02-10 19:54:17 +0700

    Making the encapsulation % constructor

--- Simple Safe/src/simple/safe/SimpleSafe.java
+++ Simple Safe/src/simple/safe/SimpleSafe.java
@@ -1,21 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package simple.safe;
 
-/**
- *
- * @author William
- */
-public class SimpleSafe {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+class RekeningBank {
+    private String namaPemilik;
+    private double saldo;
+    private String pin;
+    
+    public RekeningBank (String namaPemilik, double saldo, String pin) {
+        this.namaPemilik = namaPemilik;
+        this.saldo = saldo;
+        this.pin = pin;
     }
     
+    
+    
+    
+}
+public class SimpleSafe {
+    public static void main(String[] args) {
+        
+    }
+
 }
