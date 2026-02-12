c6a971a90bacb6e019cb4c7c5681edcb3ef573a9
Willy-557 <williamfsetiadi@gmail.com>
2026-02-12 22:49:27 +0700

    Make void updatePasar()

--- Simple Stock Market/src/simple/stock/market/SimpleStockMarket.java
+++ Simple Stock Market/src/simple/stock/market/SimpleStockMarket.java
@@ -18,7 +18,16 @@ class Saham {
         this.hargaSaham = hargaSaham;
     } 
 
-    
+    public void updatePasar() {
+        int r1 = r.nextInt(1001) - 500;
+        this.hargaSaham += r1;
+
+        if (r1 < 50) {
+            this.hargaSaham = 50;
+        }
+
+        System.out.println("Saham " + this.kodeSaham + " : Rp " + (int)this.hargaSaham + ".");
+    }
 }  
 /**
  *
